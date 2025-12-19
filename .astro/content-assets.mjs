eb4909da22b043d2025f5410acd23454bca295e0
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2025-12-19 18:19:49 +0200

    ready

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_1SjmwC from "/src/images/blog/1.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md";
-export default new Map([["/src/images/blog/1.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F1.md", __ASTRO_IMAGE_IMPORT_1SjmwC]]);
-		
+export default new Map();
--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -1,15 +1,4 @@
 declare module 'astro:content' {
-	interface Render {
-		'.mdx': Promise<{
-			Content: import('astro').MDXContent;
-			headings: import('astro').MarkdownHeading[];
-			remarkPluginFrontmatter: Record<string, any>;
-			components: import('astro').MDXInstance<{}>['components'];
-		}>;
-	}
-}
-
-declare module 'astro:content' {
 	export interface RenderResult {
 		Content: import('astro/runtime/server/index.js').AstroComponentFactory;
 		headings: import('astro').MarkdownHeading[];
@@ -173,39 +162,7 @@ declare module 'astro:content' {
 	};
 
 	type DataEntryMap = {
-		"infopages": Record<string, {
-  id: string;
-  body?: string;
-  collection: "infopages";
-  data: any;
-  rendered?: RenderedContent;
-  filePath?: string;
-}>;
-"posts": Record<string, {
-  id: string;
-  body?: string;
-  collection: "posts";
-  data: any;
-  rendered?: RenderedContent;
-  filePath?: string;
-}>;
-"simple": Record<string, {
-  id: string;
-  body?: string;
-  collection: "simple";
-  data: any;
-  rendered?: RenderedContent;
-  filePath?: string;
-}>;
-"team": Record<string, {
-  id: string;
-  body?: string;
-  collection: "team";
-  data: any;
-  rendered?: RenderedContent;
-  filePath?: string;
-}>;
-
+		
 	};
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
@@ -237,6 +194,6 @@ declare module 'astro:content' {
 		LiveContentConfig['collections'][C]['loader']
 	>;
 
-	export type ContentConfig = typeof import("../src/content/config.js");
+	export type ContentConfig = typeof import("../src/content.config.mjs");
 	export type LiveContentConfig = never;
 }
